f10ddfd18f4f5ae4dc86fd826a197ef9dfc981e2
Dev Cyth <dev.cyth@gmail.com>
2026-04-07 11:22:36 +0200

    check token middleware cannot read split fixed

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -2,7 +2,7 @@ import jwt from 'jsonwebtoken'
 
 
 export const checkToken = async (req, res, next) => {
-    const token = req.headers.authorization.split(' ')[1]
+    const token = req.headers.authorization?.split(' ')[1]
     console.log(token)
     try{
         const decoded = await jwt.verify(token, process.env.JWT_SECRET)
